1df04cfcdfad9b6096fdf26218a324f18dc997c5
S0mbr3 <benjamin_denis@outlook.com>
2023-02-01 13:29:45 +0100

    refactored and addition of automatic and dynamic layouting fitting the space automatically correctly with three layouts available on wezterm

--- .dotfiles/wezterm/.wezterm.lua
+++ .dotfiles/wezterm/.wezterm.lua
@@ -1,18 +1,23 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
-local layouts = 'top'
+--local layouts = ['top', 'right', 'column']
+local layouts_name = { 'bottom', 'right', 'column' }
+local layouts = {}
+local layouts_index = 1
+local width_cell = nil
+local height_cell = nil
 --local l = require('layouts')
 
 function getCommand(commandVar, ifnil)
-      local handle = io.popen(commandVar)
-      local output = nil
-      if handle  then
-      output = handle:read("*a")
-      handle:close()
-    else
-      output = ifnil
-    end
-    return output
+  local handle = io.popen(commandVar)
+  local output = nil
+  if handle  then
+    output = handle:read("*a")
+    handle:close()
+  else
+    output = ifnil
+  end
+  return output
 end
 
 
@@ -20,20 +25,22 @@ local move = function(id)
   local _,_,_ = wezterm.run_child_process{'wezterm', 'cli', 'move-pane-to-new-tab', '--pane-id', id}
 end
 
-  local split = function(position, id)
-    id = id or nil
-    if id == nil then
+local split = function(position, id, percentage)
+  id = id or nil
+  if id == nil then
     local _,_,_ = wezterm.run_child_process {'wezterm', 'cli', 'split-pane', position}
   else
-    local _,_,_ = wezterm.run_child_process {'wezterm', 'cli', 'split-pane', position, '--move-pane-id', id}
+    local _,_,_ = wezterm.run_child_process {'wezterm', 'cli', 'split-pane', position, '--move-pane-id', id, '--percent', percentage}
   end
 end
 
-local right = function(pane, id, first_split)
-  id = id or nil
-  first_split = first_split or nil
-  print("first split is: "..tostring(first_split))
-  if id == nil then
+local right = function(args)
+  print("entering")
+  id = args.id or nil
+  first_split = args.first_split
+  pane = args.pane or nil
+  print("first split is: "..tostring(args.first_split))
+  if args.id == nil then
     if #pane:tab():panes() <= 1 then
       --pane:split {direction = 'Right'}
       split('--right')
@@ -42,22 +49,21 @@ local right = function(pane, id, first_split)
       split('--bottom')
     end
   else
-    if first_split == true then
+    if args.first_split then
+      print("OHHHH OUUUUIIIIIII")
       --pane:split {direction = 'Right'}
-      split('--right', id)
+      split('--right', args.id, args.first_percentage)
     else
+      print('OHHHH NOOOOON'..tostring(args.first_split))
       --pane:split {direction = 'Bottom'}
-      split('--bottom', id)
+      split('--bottom', args.id, args.percentage)
     end
   end
 end
 
-local bottom = function (pane, id, first_split)
-  id = id or nil
-  first_split = first_split or nil
-  print("first split is" ..tostring(first_split))
-  if id == nil then
-    if #pane:tab():panes() <= 1 then
+local bottom = function (args)
+  if args.id == nil then
+    if #args.pane:tab():panes() <= 1 then
       split('--bottom')
       --pane:split {direction = 'Bottom'}
     else
@@ -65,151 +71,192 @@ local bottom = function (pane, id, first_split)
       --pane:split {direction = 'Right'}
     end
   else
-    if first_split == true then
-      split('--bottom', id)
+    if args.first_split then
+      split('--bottom', args.id, args.first_percentage)
       --pane:split {direction = 'Bottom'}
     else
-      split('--right', id)
+      split('--right', args.id, args.percentage)
       --pane:split {direction = 'Right'}
     end
   end
 end
+
+local column = function(args)
+  print("column layout")
+  if args.id == nil then 
+    split ('--right')
+  else
+    split('--right', args.id, args.percentage)
+  end
+end
+layouts["bottom"] = bottom
+layouts["right"] = right
+layouts["column"] = column
 
 local detect_tab = function(tabs, window, pane)
   local index=0
   for _, v in ipairs(tabs) do
-  print("is active? "..tostring(v.is_active))
-  if v.is_active == true then
+    print("is active? "..tostring(v.is_active))
+    if v.is_active == true then
       print('C EST LA BONNNNNE TAB son id est: '.. tostring(v.index))
       if v.is_active == true then print('C EST LA BONNNNNE TAB son id est: '.. v.index) end
       index = v.index
-      end
     end
-    local step = tabs[#tabs].index - index + 1
-    print("the step is: "..tostring(step))
-    window:perform_action(wezterm.action.ActivateTabRelative(step), pane)
-    window:perform_action(wezterm.action.MoveTabRelative(-step), pane)
+  end
+  local step = tabs[#tabs].index - index + 1
+  print("the step is: "..tostring(step))
+  window:perform_action(wezterm.action.ActivateTabRelative(step), pane)
+  window:perform_action(wezterm.action.MoveTabRelative(-step), pane)
 end
 
 local change_layout_bottom = function(pane_ids, pane, window)
+  local args = {pane=pane, first_split=true, percentage=100, first_percentage=50}
+  local available_space = 100
+  local size = #pane_ids
+  local gap = 0
   local first_pane = true
-  local first_split = true
   local tabs = pane:tab():window():tabs_with_info()
-  --print(tabs)
   for _, v in ipairs(pane_ids) do
     local v_string = tostring(v)
-    if first_pane == true then
+    if first_pane then
+      print('ya moa dabord')
       move(v)
       detect_tab(tabs, window, pane)
       first_pane = false
     else
-      local panes_number = #pane:tab():panes()
-      print("deuxieme tour: "..v_string)
-      print("deuxieme tour: "..tostring(first_split))
-      if layouts == "top" then
-        print("layouting: "..panes_number.. tostring(first_split))
-      bottom(pane, v_string, first_split)
-    else
-      print("layouting: "..panes_number.. " "..tostring(first_split))
-      right(pane,v_string, first_split )
+
+      print("pourcentage vaut: ".. args.percentage .. " nombres de panes: " .. size .. " le gap est de : " .. gap .. " la taille disponible est de: " .. available_space)
+      gap = math.floor(available_space / size)
+      if args.percentage > 50 then args.percentage = available_space - gap end
+      size = size - 1
+      args.id=v_string
+      layouts[layouts_name[layouts_index]](args)
+      print('que les panes ids: ' .. tostring(#pane_ids))
+      --available_space = available_space - args.percentage
+      --layouts[layouts_name[1]](args)
+      if args.first_split == true then
+        args.first_split = false
+      end
     end
-    if first_split == true then first_split = false end
-  end
   end
 end
 
+local layout_incrementer = function()
+  --layout_index = (layouts_index == #layouts_name) and 1 or layouts_index +1
+  if layouts_index == #layouts_name then
+    layouts_index = 1
+  else layouts_index = layouts_index + 1
+    -- print("layouts_index = "..tostring(layouts_index).. " la longueur de la liste = " ..#layouts_name)
+    -- print(layouts_name[1])
+  end
+end
 wezterm.on ("flayouts",
 function(window, pane)
-  if layouts == 'right' then
-    layouts = 'top'
-  elseif layouts == 'top' then
-    layouts = 'right'
-    end
   --act.MoveTabRelative()
   --window:perform_action(wezterm.action.MoveTabRelative(-1), pane)
   --local _,tabs,_ = wezterm.run_child_process {"jq", "'.[]", "|", "select(.window_id==0)", "|", ".pane_id'"}
   print("tab id is: "..pane:tab():tab_id())
-  --print( pane:tab():panes_with_info() )
-  print(pane:tab():window():tabs_with_info())
+  print( pane:tab():panes_with_info() )
+  --print(pane:tab():window():tabs_with_info())
   local panes = pane:tab():panes()
 
   local pane_ids = {}
   for _, v in ipairs(panes) do
-      print(v)
-  local v_string = tostring(v)
+    --print(v)
+    local v_string = tostring(v)
     for pane_id in string.gmatch(v_string, "pane_id:(%d+)") do
-        table.insert(pane_ids, pane_id)
+      table.insert(pane_ids, pane_id)
     end
+  end
+  -- width_cell = pane:tab():panes_with_info()[1].width
+  -- height_cell = pane:tab():panes_with_info()[1].height
+  for _, v in ipairs(pane_ids) do
+    print("les ids sont: " .. tostring(v))
   end
   change_layout_bottom(pane_ids, pane, window)
 
   --local _,tabs,_ = wezterm.run_child_process {'/bin/bash $HOME/wez.sh', pane:tab():tab_id()}
   --local tabs = getCommand('/bin/bash $HOME/wez.sh', "deso")
   --print("tabs lists: " ..tabs)
-  end);
+  layout_incrementer()
+  --print("First Spawn " .. tostring(width_cell) .." ".. tostring(height_cell) )
+end);
 
 wezterm.on("make-layouts",
 function(window, pane)
   --local panes = pane:tab():panes()
-  print("layouts tests")
-  print(#pane:tab():panes())
-  print(pane:tab():panes())
+  -- print("layouts tests")
+  -- print(#pane:tab():panes())
+  -- print(pane:tab():panes())
   print(pane:tab():panes_with_info())
-  if layouts == "right" then
-    right(pane)
-  elseif layouts == "top" then
-    bottom(pane)
+  if layouts_name[layouts_index] then
+    -- print("le nom est: "..tostring(layouts_name[layouts_index].. "son index est:".. tostring(layouts_index)))
+  end
+
+  layout = layouts[layouts_name[layouts_index]]
+  if layout then 
+    layout({pane=pane})
+  else
+    print("MEGA ERROR: ")
+    print(layouts_name)
+    print(layouts_index)
   end
 end);
 
 wezterm.on("update-status", function(window, pane)
-      -- demonstrates shelling out to get some external status.
-      -- wezterm will parse escape sequences output by the
-      -- child process and include them in the status area, too.
-      --local success, stdout, stderr = wezterm.run_child_process { 'jq', '-r', '.name', '/home/oxhart/scripts/fete/day.json' }
-      --local success, user, stderr = wezterm.run_child_process {'echo',os.getenv('USER')}
-      -- user = string.gsub(user, "\n", "")
-      -- print(string.format("%s %s %s", stdout, success, stderr))
-      local command= "jq -r '.name' $HOME/scripts/fete/day.json"
+  -- demonstrates shelling out to get some external status.
+  -- wezterm will parse escape sequences output by the
+  -- child process and include them in the status area, too.
+  --local success, stdout, stderr = wezterm.run_child_process { 'jq', '-r', '.name', '/home/oxhart/scripts/fete/day.json' }
+  --local success, user, stderr = wezterm.run_child_process {'echo',os.getenv('USER')}
+  -- user = string.gsub(user, "\n", "")
+  -- print(string.format("%s %s %s", stdout, success, stderr))
+  local command= "jq -r '.name' $HOME/scripts/fete/day.json"
 
-      local panes_number
-      if #pane:tab():panes() then panes_number = tostring(#pane:tab():panes())
-      else panes_number = "oncpa" end
+  if pane == nil then return end
+  if pane:tab() == nil then return end
+  if pane:tab():panes() == nil then return end
+  local panes_number
+  if #pane:tab():panes() then panes_number = tostring(#pane:tab():panes())
+  else panes_number = "oncpa" end
 
-      --local output = getCommand(command, "dulamour")
-      --local  success1, uname1, stderr = wezterm.run_child_process { 'uname', '-a', '|', 'cut', '-d', "' '", '-f2,3' }
-      --print("the uname1 is: "..uname1.. "and success is: "..tostring(success1))
-      local command1 = "uname -a | cut -d' ' -f2,3"
-      --local command2= "echo $USER"
-      local uname = getCommand(command1, "Oxidow")
-      uname = string.gsub(uname, "\n", "")
-      --local user = "oxhart"
-      local fete = getCommand(command, "lamour")
-      fete = string.gsub(fete, "\n", "")
+  --local output = getCommand(command, "dulamour")
+  --local  success1, uname1, stderr = wezterm.run_child_process { 'uname', '-a', '|', 'cut', '-d', "' '", '-f2,3' }
+  --print("the uname1 is: "..uname1.. "and success is: "..tostring(success1))
+  local command1 = "uname -a | cut -d' ' -f2,3"
+  --local command2= "echo $USER"
+  local uname = getCommand(command1, "Oxidow")
+  uname = string.gsub(uname, "\n", "")
+  --local user = "oxhart"
+  local fete = getCommand(command, "lamour")
+  fete = string.gsub(fete, "\n", "")
 
-      local success_date, date, stderr_date = wezterm.run_child_process({"date"});
-      if success_date then
-       date = wezterm.strftime("%A %H:%M:%S %d-%m-%Y");
-      else
-        print(stderr_date)
-      end
+  local success_date, date, stderr_date = wezterm.run_child_process({"date"});
+  if success_date then
+    date = wezterm.strftime("%A %H:%M:%S %d-%m-%Y");
+  else
+    print(stderr_date)
+  end
 
-     -- However, if all you need is to format the date/time, then:
+  -- However, if all you need is to format the date/time, then:
 
-      -- Make it italic and underlined
-      window:set_right_status(wezterm.format({
-        {Attribute={Underline="Curly"}},
-        {Attribute={Italic=true}},
-        {Text = layouts .. " " ..panes_number .." " ..os.getenv('USER').. " " ..uname.. " "},
-        {Foreground = {AnsiColor = 'Purple' }},
-        {Text = date.. " "},
-        {Foreground = {AnsiColor = 'Lime' }},
-        {Text=string.format("bonne fête aux %s", fete)},
-        -- {Text=date},
-        -- {Text=user},
-        -- {Text=fete},
-       --{Text=date.. " bonne fete aux " ..fete.. " ".. user},
-    }));
+  -- Make it italic and underlined
+  if not layouts_name[layouts_index] then
+    return
+  end
+  window:set_right_status(wezterm.format({
+    {Attribute={Underline="Curly"}},
+    {Attribute={Italic=true}},
+    {Text = layouts_name[layouts_index] .. " " ..tostring(layouts_index).. " "..panes_number .." " ..os.getenv('USER').. " " ..uname.. " "},
+    {Foreground = {AnsiColor = 'Purple' }},
+    {Text = date.. " "},
+    {Foreground = {AnsiColor = 'Lime' }},
+    {Text=string.format("bonne fête aux %s", fete)},
+    -- {Text=date},
+    -- {Text=user},
+    -- {Text=fete},
+    --{Text=date.. " bonne fete aux " ..fete.. " ".. user},
+  }));
 end);
 return {
   ssh_domains = {
