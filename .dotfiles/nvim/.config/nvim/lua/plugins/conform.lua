be8888407587a2cf173c98e5fadec6af3c0c9b3d
Benjamin <benjamin_denis@outlook.com>
2024-11-13 15:53:02 -0300

    Neovim removed save_on_format for conform.lua (it saves to every file types) create ftplugin instead

--- .dotfiles/nvim/.config/nvim/lua/plugins/conform.lua
+++ .dotfiles/nvim/.config/nvim/lua/plugins/conform.lua
@@ -16,11 +16,11 @@ return {
           end,
         },
       },
-      format_on_save = {
+      --[[ format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_format = "fallback",
-      },
+      }, ]]
     },
   },
 }
