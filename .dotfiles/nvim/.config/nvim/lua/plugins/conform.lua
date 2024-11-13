84ebb4cee5498f1f89c0bc29a52a5905f6b64d48
Benjamin <benjamin_denis@outlook.com>
2024-11-13 15:34:31 -0300

    Neovim add conform.lua + caddy formatting setup

---
+++ .dotfiles/nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["_"] = { "caddy_fmt" },
+      },
+
+      formatters = {
+        caddy_fmt = {
+          command = "caddy",
+          args = { "fmt" },
+          stdin = true,
+          condition = function(ctx)
+            return vim.fs.basename(ctx.filename) ~= "Caddyfile"
+          end,
+        },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
+}
