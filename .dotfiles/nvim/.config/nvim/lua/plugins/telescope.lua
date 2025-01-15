83b194da91d65c165bff10c61f1bca657e00a3ce
S0mbr3 <benjamin_denis@outlook.com>
2025-01-15 06:43:58 +0100

    neovim: better telescope config to handle fresh installations

--- .dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
+++ .dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,3 @@
-local actions = require("telescope.actions")
-local builtin = require("telescope.builtin")
-local extensions = require("telescope").extensions
 --local path= "~/Documents/builds/terminalConfigs/.dotfiles/"
 local path= "~/terminalConfigs/.dotfiles/"
 
@@ -19,61 +16,68 @@ return {
         module="telescope._extensions.luasnip"},
       {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
     },
-    opts = {
-      pickers = {
-        -- find_files = {
-        --   find_command = { "rg", "--ignore", "-L", "--hidden", "--files"}
-        -- }
-        find_files = {
-          find_command = { "rg", "--ignore", "-L", "--hidden", "--files", "--glob", "!node_modules/*", "--glob", "!.git/*"}
-        }
-      },
-      defaults = {
-        file_ignore_patterns = {"%.git/"},
-        mappings = {
-          i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
+    opts = function()
+      local actions = require("telescope.actions")
+      return {
+        pickers = {
+          -- find_files = {
+          --   find_command = { "rg", "--ignore", "-L", "--hidden", "--files"}
+          -- }
+          find_files = {
+            find_command = { "rg", "--ignore", "-L", "--hidden", "--files", "--glob", "!node_modules/*", "--glob", "!.git/*"}
+          }
+        },
+        defaults = {
+          file_ignore_patterns = {"%.git/"},
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
           },
         },
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,                    -- false will only do exact matching
-          override_generic_sorter = true,  -- override the generic sorter
-          override_file_sorter = true,     -- override the file sorter
-          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
-        },
-        project = {
-          hidden_files = true,
-        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          },
+          project = {
+            hidden_files = true,
+          },
+        }
       }
-    },
-    keys = {
-      {'<leader>fa',
-        function () builtin.find_files{cwd = path} end,
-        desc = '[S]earch [F]iles relative'},
+    end,
+    keys = function()
+      local builtin = require("telescope.builtin")
+      local extensions = require("telescope").extensions
+      return {
+        {'<leader>fa',
+          function () builtin.find_files{cwd = path} end,
+          desc = '[S]earch [F]iles relative'},
 
-      {'<leader>fr',
-        function () builtin.live_grep{cwd = path, additional_args=rg_args} end,
-        desc = "[S]earch [Hiden] [Grep]"},
+        {'<leader>fr',
+          function () builtin.live_grep{cwd = path, additional_args=rg_args} end,
+          desc = "[S]earch [Hiden] [Grep]"},
 
-      {'<leader>fd', builtin.diagnostics, desc= '[S]earch [D]iagnostics'},
+        {'<leader>fd', builtin.diagnostics, desc= '[S]earch [D]iagnostics'},
 
-      {'<leader>fo', builtin.oldfiles, desc = '[S]earch [R]ecently [O]ened [F]iles'},
+        {'<leader>fo', builtin.oldfiles, desc = '[S]earch [R]ecently [O]ened [F]iles'},
 
-      {'<leader>ff', function() builtin.find_files() end, desc = "[F]ind [Files] in current directory"},
-      {'<leader>fb', function() builtin.buffers() end, desc = "[S]earch opened [Buffers]"},
-      {'\\f', function() builtin.find_files() end, desc = "[F]ind [Files] in current directory"},
-      {'\\b', function() builtin.buffers() end, desc = "[S]earch opened [Buffers]"},
-      {'<leader>fg', function() builtin.live_grep() end, desc = "[G]rep in current directory"},
-      {'<leader>fh', function() builtin.help_tags() end, desc = "[Search] items in the nvim [Help]"},
-      {'<leader>fc', function() extensions.project.project{} end},
-      {'<leader>fn', function() extensions.file_browser.file_browser{} end},
-      {'<leader>fe', function() builtin.builtin({include_extensions=true})end}
+        {'<leader>ff', function() builtin.find_files() end, desc = "[F]ind [Files] in current directory"},
+        {'<leader>fb', function() builtin.buffers() end, desc = "[S]earch opened [Buffers]"},
+        {'\\f', function() builtin.find_files() end, desc = "[F]ind [Files] in current directory"},
+        {'\\b', function() builtin.buffers() end, desc = "[S]earch opened [Buffers]"},
+        {'<leader>fg', function() builtin.live_grep() end, desc = "[G]rep in current directory"},
+        {'<leader>fh', function() builtin.help_tags() end, desc = "[Search] items in the nvim [Help]"},
+        {'<leader>fc', function() extensions.project.project{} end},
+        {'<leader>fn', function() extensions.file_browser.file_browser{} end},
+        {'<leader>fe', function() builtin.builtin({include_extensions=true})end}
 
-    },
+      }
+    end,
     config = function(_, opts)
       require'telescope'.setup(opts)
       -- To get fzf loaded and working with telescope, you need to call
