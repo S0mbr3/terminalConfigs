6503207cdafd05399e3c448c937370ccb9a18b4a
S0mbr3 <benjamin_denis@outlook.com>
2025-02-13 21:28:08 +0100

    nixos: reformat home.nix

--- .dotfiles/nix/home.nix
+++ .dotfiles/nix/home.nix
@@ -1,29 +1,33 @@
-{ pkgs }:
-with pkgs;
-{
+{ pkgs, ... }:
 
   # * Home Manager
   # use home manager as nix-darwin module, so that user profiles are built
   # together with the system when running darwin-rebuild
-  home-manager = {
-    useGlobalPkgs = true;
-    users.${user} = {
+{
+  home.username = "nebj";
+  home.homeDirectory = "/Users/nebj";
       home.enableNixpkgsReleaseCheck = false;
-      home.packages = pkgs.callPackage ./packages.nix {};
+      # home.packages = pkgs.callPackage ./packages.nix {};
+      # home.packages = with pkgs;[
+      #   home-manager
+      #   ];
 
       home.stateVersion = "25.05";
 
       # extra directories to add to path
       home.sessionPath = [
-        "${user}/bin"
+        "$HOME/bin"
       ];
       programs.neovim = {
               enable = true;
               extraLuaPackages = ps: [ ps.magick ];
               extraPackages = [ pkgs.imagemagick ];
           };
-      programs.zsh.enable = true;
+      programs.home-manager.enable = true;
+      programs.zsh.enable = false;
 
-    };
-  };
-}
+      home.file.".emacs.d" = {
+        source = ../emacs/.emacs.d;
+      };
+    }
+
