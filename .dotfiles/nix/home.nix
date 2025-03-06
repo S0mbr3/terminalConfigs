fea62c5dec511eac8ba85c9fa03e1eed7888e624
S0mbr3 <benjamin_denis@outlook.com>
2025-03-06 22:58:14 +0100

    nix: add syncthing service to home-manager

--- .dotfiles/nix/home.nix
+++ .dotfiles/nix/home.nix
@@ -24,4 +24,66 @@ rec {
       home.file.".emacs.d" = {
         source = ../emacs/.emacs.d;
       };
+
+   home.activation.decryptPassword = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.age}/bin/age --decrypt -i ~/.config/sops/age/keys.txt ${config.home.homeDirectory}/terminalConfigs/.dotfiles/nix/.passwordFile.age > ${config.home.homeDirectory}/.passwordFile
+  '';
+
+    services = {
+    syncthing = {
+      enable = true;
+      #passwordFile = ./password;
+      passwordFile = "${config.home.homeDirectory}/.passwordFile";
+      overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+      overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+      settings = {
+	options = {
+	  urAccepted = -1;
+	};
+	devices = {
+	  nebj = {
+	    name = "nebj";
+	    id = "JXZ3KCU-NCNNCU3-ZQK2UZ6-VHDYHVP-I2FUBUN-E4DTAAB-VNVHKVO-LYAXSAB";
+	    autoAcceptFolders = true;
+	  };
+	  android = {
+	    name = "android";
+	    id = "A5KISFU-IBH7OH2-7OLMT7V-3YCDSRP-VCLXWLR-ECBRQNF-MP5PNSO-ZJSD3AA";
+	    autoAcceptFolders = true;
+	  };
+	};
+	# folders = {
+	#   "org" = {
+	#     path = "~/org";
+	#     devices = ["nebj" "android"];
+	#   };
+	# };
+	gui = {
+	  user = config.rage-syncthingUser;
+	};
+      };
+      #extraOptions = ["cli config user set patata "];
+    };
+  };
+  launchd.agents.removePassword = {
+    enable = false;
+    config = {
+      ProgramArguments = [
+	"${pkgs.bash}/bin/bash"
+	"-c"
+	''
+	  while ! pgrep syncthing >/dev/null; do sleep 1; done
+	  sleep 5
+          ${pkgs.coreutils}/bin/shred -u ${config.home.homeDirectory}/.passwordFile
+	''
+      ];
+      Label = "com.yourdomain.removePassword";
+      StartInterval = 10;  # Runs every 10 seconds, modify as needed
+      RunAtLoad = true;    # Runs at startup
+      KeepAlive = false;   # Don't restart once it runs
+    StandardOutPath = "/tmp/removePassword.out.log";
+    StandardErrorPath = "/tmp/removePassword.err.log";
+    };
+  };
+
     }
