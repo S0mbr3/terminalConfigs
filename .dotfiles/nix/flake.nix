2d08036b7a1818af125fdd27eb8c9668b5948e41
S0mbr3 <benjamin_denis@outlook.com>
2025-04-13 13:11:15 +0200

    nixos: enable backing up directories through home-manager while switching

--- .dotfiles/nix/flake.nix
+++ .dotfiles/nix/flake.nix
@@ -105,6 +105,7 @@
           home-manager.darwinModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+	    home-manager.backupFileExtension = "Backup";
 	    home-manager.users.${rage-username}.imports = [ ./home.nix secrets ];
           }
           ./darwin
