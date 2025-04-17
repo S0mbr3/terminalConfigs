f83a7a7ecf376afda7ad6aef0d067576bd04e21b
S0mbr3 <benjamin_denis@outlook.com>
2025-04-17 16:42:29 +0200

    nixos: include home.username into home.nix to allow using home-manager in the cli

--- .dotfiles/nix/home.nix
+++ .dotfiles/nix/home.nix
@@ -5,6 +5,7 @@
   # use home manager as nix-darwin module, so that user profiles are built
   # together with the system when running darwin-rebuild
 rec {
+  home.username = config.rage-username;
   home.homeDirectory = builtins.trace "configdir: ${config.home.username}" "/Users/${config.home.username}";
       home.enableNixpkgsReleaseCheck = false;
       home.stateVersion = "25.05";
